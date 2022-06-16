ceade162df1c573655f67db4a236a05e711f7a9a
John <john@johntelford.com>
2022-06-15 17:12:51 -0700

    filelastmodified
    Signed-off-by: John <john@johntelford.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,8 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
+          showLastUpdateTime: true,
+          breadcrumbs: true,
           // Please change this to your repo.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
